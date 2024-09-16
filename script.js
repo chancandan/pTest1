ec7c383671771698581240bd5023018861bf806e
chancandan <daniellyons@live.ie>
2024-09-16 15:00:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optional script to enable smooth scroll behavior for modern browsers
+document.addEventListener("DOMContentLoaded", function() {
+    document.documentElement.style.scrollBehavior = "smooth";
+});
